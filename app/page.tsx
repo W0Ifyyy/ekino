6971323ad39e68511ea3916ab0b55f383be331c3
W0Ifyyy <bartosz.wilk.123@hotmail.com>
2025-06-10 19:41:09 +0200

    konfiguracja tailwindcss i shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
-  return <main>Hello</main>;
+  return (
+    <main>
+      <Button>Przycisk</Button>
+    </main>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          "2xl": "1200px",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
